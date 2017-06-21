8df8467666b0aef1b8ea7132c302e00482a4bdb2
etsracas <etsryacas@gmail.com>
2017-06-21 20:46:38 +0900

    chapter6

---
+++ client/app/components/MessageForm/index.js
@@ -0,0 +1,11 @@
+import React, { PropTypes } from 'react'
+
+const MessageForm = () => {
+  return (
+    <div>
+      <textarea rows="4" cols="40" />
+    </div>
+  )
+}
+
+export default MessageForm
