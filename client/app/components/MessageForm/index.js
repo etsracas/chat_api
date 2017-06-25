926a92ef8952567c210f07661dbb1f06f5a41071
etsracas <etsryacas@gmail.com>
2017-06-25 22:12:39 +0900

    chapter26

--- client/app/components/MessageForm/index.js
+++ client/app/components/MessageForm/index.js
@@ -14,13 +14,25 @@ class MessageForm extends Component {
     })
   }
 
+  onUpdate() {
+    const { actions, message } = this.props
+    $.ajax({
+      type: 'PATCH',
+      url: '/api/messages/' + message.id,
+      data: {
+        message: {text: this.textMessage.value}
+      },
+      success: ((data) => actions.updateMessage(data))
+    })
+  }
+
   render() {
     const { message, onClick } = this.props
     const cancelButton = message ? <input type='button' value='キャンセル' onClick={onClick}/> : null
     return (
       <div>
         <textarea rows="4" cols="40" ref={(input) => {this.textMessage = input}} defaultValue={message ? message.text : ''} />
-        <input type='button' value={message ? '更新' : '登録'} onClick={this.onSubmit.bind(this)} />
+        <input type='button' value={message ? '更新' : '登録'} onClick={message ? this.onUpdate.bind(this) : this.onSubmit.bind(this)} />
         {cancelButton}
       </div>
     )
