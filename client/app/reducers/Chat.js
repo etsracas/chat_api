4e725e7a9530ba648a015771447722e0c8fe427a
etsracas <etsryacas@gmail.com>
2017-06-25 16:42:38 +0900

    chpater18

--- client/app/reducers/Chat.js
+++ client/app/reducers/Chat.js
@@ -1,7 +1,14 @@
 import { combineReducers } from 'redux'
-import { ADD_MESSAGE, CREATE_MESSAGE_LIST} from '../constants/chat'
+import { ADD_MESSAGE, CREATE_MESSAGE_LIST, CHANGE_DISPLAY_STATE} from '../constants/chat'
 
 export const chatInitialState = [{"id":2,"text":"message2"},{"id":1,"text":"message1"}]
+
+const toggleMessage = (id, message) => {
+  if(id == message.id) {
+    message.displayState = !message.displayState
+  }
+  return message
+}
 
 const chat = (state = '', action) => {
   switch (action.type) {
@@ -14,6 +21,8 @@ const chat = (state = '', action) => {
         message['displayState'] = true
         return message
       })
+    case CHANGE_DISPLAY_STATE:
+      return state.map((message) => toggleMessage(action.id, message))
     default:
       return state
   }
